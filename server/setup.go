06615226779b4f5d954bd33a579f2ca36bd2ae94
nitohu <nitohu@gmail.com>
2020-07-25 15:50:06 +0200

    Removed unused function, fixed wrong style for select in settings page

--- server/setup.go
+++ server/setup.go
@@ -4,36 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
-	"text/template"
 
 	"github.com/nitohu/err"
 )
-
-func initTemplates(files ...string) (*template.Template, error) {
-	workingDir, err := os.Getwd()
-
-	if err != nil {
-		fmt.Printf("[ERROR] %s", err)
-	}
-
-	var paths []string
-
-	if !strings.HasSuffix(workingDir, "/server") {
-		workingDir += "/server"
-	}
-
-	for i := range files {
-		file := files[i]
-		path := workingDir + "/templates/" + file
-
-		paths = append(paths, path)
-	}
-
-	tmpl, err := template.ParseFiles(paths...)
-
-	return tmpl, err
-}
 
 func getCmdLineArgs(args []string) (map[string]string, err.Error) {
 	if len(args) <= 1 {
